9a690cbb9d28bdde02ed6b3324b199905df06ea5
Aliaksandr_Kananenka_ <Aliaksandr_Kananenka@epam.com>
2020-11-07 21:47:09 +0300

    Add test.

---
+++ ContinuousDeployment.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using ContinuousDeployment.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace ContinuousDeployment.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_WeatherForecast_ShouldWorkAsExpected()
+        {
+            // Arrange
+
+            var logger = new Mock<ILogger<WeatherForecastController>>();
+
+            WeatherForecastController controller = new WeatherForecastController(logger.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotEmpty(result);
+        }
+    }
+}
